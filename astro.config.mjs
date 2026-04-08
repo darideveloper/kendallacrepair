8752eb2f93bce78afded38f939073c84c3b30d10
daridev <darideveloper@gmail.com>
2026-04-08 09:46:35 -0600

    feat: project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import tailwindcss from "@tailwindcss/vite"
+
+import showTailwindcssBreakpoint from "astro-show-tailwindcss-breakpoint"
+
+import react from "@astrojs/react"
+
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: 'https://kendallacrepair.com',
+  integrations: [showTailwindcssBreakpoint(), react(), sitemap()],
+})
