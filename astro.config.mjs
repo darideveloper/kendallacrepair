4be534fd1844027d5eb31f974d672957e5e4fe51
daridev <darideveloper@gmail.com>
2026-04-16 14:45:27 -0600

    feat (dev): add portless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
-
 import showTailwindcssBreakpoint from "astro-show-tailwindcss-breakpoint"
-
 import react from "@astrojs/react"
-
 import sitemap from "@astrojs/sitemap"
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      // Vite specifically needs this set to ensure the HMR 
+      // client knows which port to talk to after a restart
+      port: process.env.PORT ? parseInt(process.env.PORT) : 4321,
+      strictPort: true, 
+    }
   },
   site: 'https://kendallacrepair.com',
   integrations: [showTailwindcssBreakpoint(), react(), sitemap()],
   server: {
-    port: 4321, // Forces Astro to stay on 4321
-    strictPort: true, // Prevents Vite from switching to 5173 if 4321 is busy
+    // Astro's top-level server config
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4321,
   },
+
 })
